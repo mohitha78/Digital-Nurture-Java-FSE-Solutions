d46289aeefbdc1d517ae9db1e99659c87e3f373d
mohitha78 <mohithakommareddy7805@gmail.com>
2026-06-22 03:42:49 +0530

    Add files via upload

---
+++ 01-DesignPatterns/SingletonPatternExample/TestLogger.java
@@ -0,0 +1,20 @@
+package SingletonPatternExample;
+
+public class TestLogger {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("User Logged In");
+
+        System.out.println(logger1);
+        System.out.println(logger2);
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        }
+    }
+}
