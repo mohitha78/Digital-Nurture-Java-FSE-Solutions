fd5564836ecc757f6b088a1b8972cde7b6e7b4e3
mohitha78 <mohithakommareddy7805@gmail.com>
2026-07-09 07:00:31 +0530

    Add files via upload

---
+++ 02-DataStructuresAndAlgorithms/FinancialForecastingExample/FinancialForecast.java
@@ -0,0 +1,15 @@
+public class FinancialForecast {
+
+    // Recursive method to calculate future value
+    public static double calculateFutureValue(double presentValue, double growthRate, int years) {
+
+        // Base case
+        if (years == 0) {
+            return presentValue;
+        }
+
+        // Recursive call
+        return calculateFutureValue(presentValue, growthRate, years - 1) * (1 + growthRate);
+
+    }
+}
