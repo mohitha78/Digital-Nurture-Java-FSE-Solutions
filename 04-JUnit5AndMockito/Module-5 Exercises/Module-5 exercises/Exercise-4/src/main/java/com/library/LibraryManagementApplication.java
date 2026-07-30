22ceb690456c67931284f6a97f6c2f9365147e8c
mohitha78 <mohithakommareddy7805@gmail.com>
2026-07-30 14:46:19 +0530

    Delete 04-JUnit5AndMockito/Module-5 Exercises directory

--- 04-JUnit5AndMockito/Module-5 Exercises/Module-5 exercises/Exercise-4/src/main/java/com/library/LibraryManagementApplication.java
+++
@@ -1,7 +0,0 @@
-package com.library;
-
-public class LibraryManagementApplication {
-    public static void main(String[] args) {
-        System.out.println("Library Management Maven project configured successfully");
-    }
-}
